2189389c8afe9dba2c88aeab83f4233692cd0efc
Attila Kling <attila.kling@abbyy.com>
2023-01-14 14:20:56 +0100

    fix

--- portal/src/pages/api/echo.ts
+++ portal/src/pages/api/echo.ts
@@ -2,5 +2,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
-    res.status(200).send('This is a response from the server');
+    res.status(200).json({ message: 'This is a response from the server' });
 }
